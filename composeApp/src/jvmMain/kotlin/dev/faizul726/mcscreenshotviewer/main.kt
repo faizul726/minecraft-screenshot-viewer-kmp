e49486f23f2c27c933feba483a20df1a6b96a31e
Fzul <162413089+faizul726@users.noreply.github.com>
2026-02-25 15:12:36 +0600

    try fixing error

--- composeApp/src/jvmMain/kotlin/dev/faizul726/mcscreenshotviewer/main.kt
+++ composeApp/src/jvmMain/kotlin/dev/faizul726/mcscreenshotviewer/main.kt
@@ -3,11 +3,28 @@ package dev.faizul726.mcscreenshotviewer
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Minecraft Screenshot Viewer",
-    ) {
-        App()
+fun main() {
+    Thread.setDefaultUncaughtExceptionHandler { _, e ->
+        Dialog(Frame(), e.message ?: "Error").apply {
+            layout = FlowLayout()
+            val label = Label(e.message)
+            add(label)
+            val button = Button("OK").apply {
+                addActionListener { dispose() }
+            }
+            add(button)
+            setSize(300,300)
+            isVisible = true
+        }
+    }
+
+    application {
+        throw Exception("My custom error!")
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "Minecraft Screenshot Viewer",
+        ) {
+            App()
+        }
     }
 }
